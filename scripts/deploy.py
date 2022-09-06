c9847652b4cca4a7c5b62cff74fccb7d292b3e23
robel.michael <robel.michael@onelab.se>
2022-09-06 16:44:11 +0200

    updateFrontend script

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,4 +1,8 @@
 from brownie import accounts, config, NftMarketPlace, Auction
+import yaml
+import json
+import os
+import shutil
 
 def deployNftMarket():
     account = accounts[0]
@@ -12,6 +16,21 @@ def deployNftMarket():
     nftmarket = NftMarketPlace.deploy({"from": account})
     auction = Auction.deploy({"from": account})
     return nftmarket, account, player, buyer, secondBuyer, auction, auctionSeller, auctionBuyer, auctionHigherBid
+
+def update_front_end():
+
+    copy_folder_to_frontend("./build", "../GameDapp/game-dapp/src/chain-info")
+
+    with open("brownie-config.yaml", "r") as brownie_config:
+        config_dict = yaml.load(brownie_config, Loader=yaml.FullLoader)
+        with open("../NFTMARKET/nftmarket/brownie-config.json", "w") as brownie_config_json:
+            json.dump(config_dict, brownie_config_json)
+        print("Front end updated")
+
+def copy_folder_to_frontend(src, dest):
+    if os.path.exists(dest):
+        shutil.rmtree(dest)
+    shutil.copytree(src, dest)
 
 def main():
     deployNftMarket()
