20e711d959f6493d08fa69c2aa3c5554641309ff
robel.michael <robel.michael@onelab.se>
2022-09-06 16:57:44 +0200

    default account added

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -5,22 +5,23 @@ import os
 import shutil
 
 def deployNftMarket():
-    account = accounts[0]
-    player = accounts[1]
-    buyer = accounts[2]
-    secondBuyer = accounts[3]
-    auctionSeller = accounts[4]
-    auctionBuyer = accounts[5]
-    auctionHigherBid = accounts[6]
+    # account = accounts[0]
+    # player = accounts[1]
+    # buyer = accounts[2]
+    # secondBuyer = accounts[3]
+    # auctionSeller = accounts[4]
+    # auctionBuyer = accounts[5]
+    # auctionHigherBid = accounts[6]
 
+    account = accounts.add(config['wallets']["from_key"])
     nftmarket = NftMarketPlace.deploy({"from": account})
     auction = Auction.deploy({"from": account})
-    return nftmarket, account, player, buyer, secondBuyer, auction, auctionSeller, auctionBuyer, auctionHigherBid
+
+    return nftmarket, auction
+    #return nftmarket, account, player, buyer, secondBuyer, auction, auctionSeller, auctionBuyer, auctionHigherBid
 
 def update_front_end():
-
     copy_folder_to_frontend("./build", "../GameDapp/game-dapp/src/chain-info")
-
     with open("brownie-config.yaml", "r") as brownie_config:
         config_dict = yaml.load(brownie_config, Loader=yaml.FullLoader)
         with open("../NFTMARKET/nftmarket/brownie-config.json", "w") as brownie_config_json:
